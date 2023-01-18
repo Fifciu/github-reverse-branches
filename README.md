5c48aded463a0adef5ab1ef53b2f06ae146946ee
Fifciu <fjedrasik@vuestorefront.io>
2023-01-18 19:07:53 +0100

    feat: mvp

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Chrome Extension Boilerplate
+# GitHub Reverse Branches
+
+Simple Chrome extension reversing `base` and `compare` branches on click while creating a pull request on GitHub.
+
+Based on [this boilerplate](https://github.com/virejdasani/Chrome-Extension-Boilerplate).
+
+[Watch this](https://www.youtube.com/watch?v=_7F8sppax54&ab_channel=Virej) video to learn how to install it from package.
+
+## TODO
+
+- [ ] Rewrite to Manifest V3
+- [ ] Support for automaticly detecting main branch in the repository (now it's hardcoded `main`)
+- [ ] Publish in Chrome market
--- popup.js
+++ popup.js
@@ -1,4 +1,51 @@
-console.log("Subscribe to Virej!");
-console.log(
-  "https://www.youtube.com/channel/UCRzS48bskynAxoI38BFypfQ?sub_confirmation=1"
-);
+// const console = chrome.extension.getBackgroundPage().console;
+const regex = new RegExp("https://github.com/(.*?)/(.*?)/compare/");
+const query = { active: true, currentWindow: true };
+const mainBranch = 'main'; // TODO: figuring out
+
+const canSwitchOnTab = tab => {
+  //https://github.com/*/*/compare/
+  if (!tab.url.startsWith('https://github.com')) {
+    return false
+  }
+  return Boolean(tab.url.match(regex));
+};
+
+// canSwitchOnTab({ url: 'https://github.com/vuestorefront/commercetools/compare/develop?expand=1' }) //?
+
+
+const switchBtn = document.querySelector('#switchBtn');
+const btnOnClick = () => {
+  chrome.tabs.query(query, ([tab, ]) => {
+    const currentUrl = new URL(tab.url);
+    const pathnameParts = currentUrl.pathname.split('/')
+    const branchesInUrl = pathnameParts.pop();
+    if (branchesInUrl.includes('...')) {
+      const [to, from] = branchesInUrl.split('...');
+      pathnameParts.push(`${from}...${to}`);
+    } else {
+      pathnameParts.push(`${branchesInUrl}...${mainBranch}`);
+    }
+    chrome.tabs.update(tab.id, {
+      url: `${currentUrl.origin}${pathnameParts.join('/')}${currentUrl.search}${currentUrl.hash}`
+    })
+  });
+};
+switchBtn.addEventListener('click', btnOnClick);
+
+window.onload = () => {
+  chrome.tabs.query(query, ([tab, ]) => {
+    if (!tab) {
+      return
+    }
+    if (!canSwitchOnTab(tab)) {
+      switchBtn.disabled = true;
+    } else {
+      switchBtn.disabled = false;
+    }
+  })
+};
+
+window.onunload = () => {
+  switchBtn.removeEventListener('click', btnOnClick);
+};
